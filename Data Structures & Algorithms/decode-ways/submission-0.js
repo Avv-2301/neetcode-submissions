b7732d6a538caf84db96e6fb7890111f06feb4da
Akshat Vijayvergiya <126230437+Avv-2301@users.noreply.github.com>
2026-09-08 12:06:12 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    numDecodings(s) {
+        const dp = new Map();
+        dp.set(s.length, 1);
+        return this.dfs(s, 0, dp);
+    }
+
+    dfs(s, i, dp) {
+        if (dp.has(i)) {
+            return dp.get(i);
+        }
+
+        if (s.charAt(i) === "0") {
+            return 0;
+        }
+
+        let res = this.dfs(s, i + 1, dp);
+        if (
+            i + 1 < s.length &&
+            (s.charAt(i) === "1" || (s.charAt(i) === "2" && s.charAt(i + 1) < "7"))
+        ) {
+            res += this.dfs(s, i + 2, dp);
+        }
+        dp.set(i, res);
+        return res;
+    }
+}
